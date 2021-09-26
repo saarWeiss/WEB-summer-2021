59e25c2b036cc8e74ebe65e59e909538f33230e2
saarWeiss <88391514+saarWeiss@users.noreply.github.com>
2021-09-26 23:37:47 +0300

    Add files via upload

---
+++ saar_wiess_app_7/db/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "sa6075725sa",
+    DB: "mysql"
+    };
---
+++ saar_wiess_app_7/public/javascripts/CV.js
@@ -0,0 +1,4 @@
+
+function myFunction(){
+document.getElementById("h1").innerHTML="Change the title content";
+}
